b6e9c65d8f6c49bcd2eed68636d926f9a5fc0f9f
Erandi Ramirez <eraonoera@gmail.com>
2026-06-22 19:29:47 -0600

    feat: agregar assets de ruta, timeline y logo de carga

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -2,8 +2,12 @@ import backIcon from '../assets/back.svg'
 import dropoffIcon from '../assets/dropoff.svg'
 import fclIcon from '../assets/fcl.svg'
 import ftlIcon from '../assets/ftl.svg'
+import completedIcon from '../assets/completed.svg'
+import holdIcon from '../assets/hold.svg'
+import logoIcon from '../assets/image.png'
 import notiIcon from '../assets/noti.svg'
 import orderCheckIcon from '../assets/orderCheck.svg'
+import orderPickIcon from '../assets/orderPick.svg'
 import pickupIcon from '../assets/pickup.svg'
 import resumeIcon from '../assets/resume.svg'
 
@@ -12,8 +16,12 @@ export const ASSETS = {
   dropoff: dropoffIcon,
   fcl: fclIcon,
   ftl: ftlIcon,
+  completed: completedIcon,
+  hold: holdIcon,
+  logo: logoIcon,
   noti: notiIcon,
   orderCheck: orderCheckIcon,
+  orderPick: orderPickIcon,
   pickup: pickupIcon,
   resume: resumeIcon,
 } as const
--- src/constants/route.ts
+++ src/constants/route.ts
@@ -4,3 +4,13 @@ export const ROUTE_ICONS = {
   pickup: { src: ASSETS.pickup, className: 'h-[15px] w-[22px]' },
   dropoff: { src: ASSETS.dropoff, className: 'h-[14px] w-[11px]' },
 } as const
+
+export const DETAIL_STOP_ICONS = {
+  active: { src: ASSETS.orderPick, className: 'h-[32px] w-[32px]' },
+  inactive: { src: ASSETS.hold, className: 'h-[32px] w-[32px]' },
+} as const
+
+export const TIMELINE_ICONS = {
+  active: { src: ASSETS.orderCheck, className: 'h-2.5 w-auto' },
+  inactive: { src: ASSETS.completed, className: 'h-7 w-7' },
+} as const
